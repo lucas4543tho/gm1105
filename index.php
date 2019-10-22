e27ee21a768d02f3fd53e02ef581df39f3dbd5a2
lucas4543tho <56897828+lucas4543tho@users.noreply.github.com>
2019-10-23 03:57:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: https://www.mcafee.com/consumer/en-us/store/m0/catalog.html");
 die();
 ?>
