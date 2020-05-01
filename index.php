cd46cdb062e7bb7587ce28f179fb165d015996a3
lucas4543tho <56897828+lucas4543tho@users.noreply.github.com>
2020-04-30 20:06:10 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-header("Location: https://www.mcafee.com/consumer/en-us/store/m0/catalog/mtp_521/mcafee-total-protection-trial.html
-");
+header("Location: https://www.candystore.com/candy-buffets/");
 die();
 ?>
